26fba80333fa06ecc2173358d5733d50e4ca3aab
ElvisFabian <elvis.fabian@sigma.com.br>
2018-11-07 10:36:56 -0300

    Create project sample

---
+++ test/TJMT.Docker.DotNet.Sample.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TJMT.Docker.DotNet.Sample.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
